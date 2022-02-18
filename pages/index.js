f940b24b31b599e81a639549bb725a8f7259860b
Kalpan Shah <kalpanshah@ymail.com>
2022-02-18 15:57:20 +1100

    changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1> Hello Next World</h1>
+      <h1> Hello Next World </h1>
     </div>
   )
 }
